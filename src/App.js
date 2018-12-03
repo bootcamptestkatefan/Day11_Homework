f28464bb0ff95914bd16f990342ac2f6dc0d5df3
bootcamptestkatefan <kate.fan@oocl.com>
2018-12-04 06:41:29 +0800

    [kate]feat: add a new button and input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import CounterGroup from './components/CounterGroup.js'
+import Counter from './components/Counter.js';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-      
+      <CounterGroup/>
+      <Counter/>
       </div>
     );
   }
